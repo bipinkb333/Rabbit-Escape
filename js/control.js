553f98ae072cff2a6ef5fba36b6fbd3f500fcbf6
bipinkb333 <bipinkb333@gmail.com>
2026-03-06 19:26:15 +0530

    Update control.js

--- js/control.js
+++ js/control.js
@@ -1,10 +1,22 @@
-export function setupControls(player){
+export function setupControls(player) {
 
-document.addEventListener("keydown",e=>{
+document.addEventListener("keydown", (e) => {
 
-if(e.key==="ArrowLeft") player.position.x -=1
+if (e.key === "ArrowLeft") {
+player.position.x -= 2
+}
 
-if(e.key==="ArrowRight") player.position.x +=1
+if (e.key === "ArrowRight") {
+player.position.x += 2
+}
+
+if (e.key === "ArrowUp") {
+player.position.z -= 2
+}
+
+if (e.key === "ArrowDown") {
+player.position.z += 2
+}
 
 })
 
