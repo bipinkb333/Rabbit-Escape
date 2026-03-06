465d517d7b1c966eacbac14820e5ef3b830b6211
bipinkb333 <bipinkb333@gmail.com>
2026-03-06 20:59:07 +0530

    Update foxAI.js

--- js/foxAI.js
+++ js/foxAI.js
@@ -1,7 +1,11 @@
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.152.2/build/three.module.js";
+let fox
+
+let foxDistance = -10   // starting distance behind rabbit
+
 export function createFox(scene){
 
-const fox = new THREE.Mesh(
+fox = new THREE.Mesh(
 
 new THREE.BoxGeometry(1,1,1),
 
@@ -9,28 +13,50 @@ new THREE.MeshStandardMaterial({color:0xff6600})
 
 )
 
-fox.position.set(0,0.5,30)
+fox.position.set(0,0.5,foxDistance)
 
 scene.add(fox)
 
 return {
+update
+}
 
-update(player){
+}
 
-fox.position.z +=0.27
+function update(player){
 
-if(fox.position.z > player.position.z-1){
+// fox slowly catches up
+foxDistance += 0.002
 
-alert("Fox caught the rabbit!")
+fox.position.z = player.position.z + foxDistance
 
-location.reload()
+fox.position.x += (player.position.x - fox.position.x) * 0.05
+
+// catch condition
+if(foxDistance > -1){
+
+showGameOver()
 
 }
 
 }
 
+function showGameOver(){
+
+const div = document.createElement("div")
+
+div.style.position="absolute"
+div.style.top="50%"
+div.style.left="50%"
+div.style.transform="translate(-50%,-50%)"
+div.style.background="black"
+div.style.color="white"
+div.style.padding="30px"
+div.style.fontSize="30px"
+div.innerText="Game Over"
+
+document.body.appendChild(div)
+
+setTimeout(()=>location.reload(),2000)
+
 }
-
-
-}
-
